1f9bb2fb6d5ce79bf88fbdd56248343d5e586392
datim <jroecks@gmail.com>
2014-10-24 12:41:32 -0700

    Add directory searching

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -11,9 +11,18 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    // jackson json will map the return object into JSON
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
+
+    // jackson json will map the return object into JSON
+    @RequestMapping("/listdir")
+    public DirTraverse listDir(@RequestParam(value="dir", defaultValue="C:\\") String directory) {
+	return new DirTraverse(directory);
+  
+    }
+
 }
