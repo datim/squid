b49a6e150ad5c20ff6a0006a747a25d67fd1831c
datim <jroecks@gmail.com>
2014-10-28 23:17:07 -0700

    Add spring cli download script

---
+++ scripts/getSprintCLI.sh
@@ -0,0 +1,9 @@
+
+echo "downloading spring-boot-cli-1.2.0.BUILD-SNAPSHOT-bin.tar.gz"
+wget http://repo.spring.io/snapshot/org/springframework/boot/spring-boot-cli/1.2.0.BUILD-SNAPSHOT/spring-boot-cli-1.2.0.BUILD-SNAPSHOT-bin.tar.gz
+
+echo "opening spring-boot-cli-1.2.0.BUILD-SNAPSHOT-bin.tar.gz"
+tar -xzvf spring-boot-cli-1.2.0.BUILD-SNAPSHOT-bin.tar.gz
+
+echo "removing spring-boot-cli-1.2.0.BUILD-SNAPSHOT-bin.tar.gz"
+rm spring-boot-cli-1.2.0.BUILD-SNAPSHOT-bin.tar.gz
