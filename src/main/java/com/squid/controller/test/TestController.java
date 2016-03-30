252897289275b113eb6330688b0c25a70731a7a0
Datim <jroecks@gmail.com>
2016-03-29 22:52:14 -0700

    Update Post command

--- src/main/java/com/squid/controller/test/TestController.java
+++ src/main/java/com/squid/controller/test/TestController.java
@@ -9,7 +9,10 @@ import com.squid.service.test.TestService;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -64,16 +67,21 @@ public class TestController {
     	BoxData data = testService.getBox(id);
     	
     	BoxDTO dto = new BoxDTO();
-    	dto.heigth = data.getHeigth();
-    	dto.length = data.getLength();
-    	dto.width = data.getWidth();
-    	dto.id = data.getId();
+
+    	if (data != null) {
+        	dto.heigth = data.getHeigth();
+        	dto.length = data.getLength();
+        	dto.width = data.getWidth();
+        	dto.id = data.getId();    	
+        	}
+    	
+
     	
     	return dto;
     }
     
     @RequestMapping(path="/box", method = RequestMethod.POST) 
-    public @ResponseBody BoxDTO setBox(@RequestParam(value="box") BoxDTO box) {
+    public @ResponseBody BoxDTO setBox(@RequestBody BoxDTO box) {
     	
     	BoxData data = new BoxData();
     	data.setHeigth(box.heigth);
--- src/main/java/com/squid/data/BoxData.java
+++ src/main/java/com/squid/data/BoxData.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "BoxData")
+@Table(name = "Box")
 public class BoxData {
 	
 	@Id
