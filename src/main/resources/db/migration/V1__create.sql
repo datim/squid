c20c59ea28b3c28b595af23514f46627e2bf717c
U-AMERICAS\roecks <roecks@ROECKS2.americas.hpqcorp.net>
2016-05-09 19:18:30 -0700

    Add preferences

--- src/main/resources/db/migration/V1__create.sql
+++ src/main/resources/db/migration/V1__create.sql
@@ -15,9 +15,38 @@ create table url_node (
 
 alter table url_node add foreign key (parent_node) references url_node(id);
 
+/*discovered photos for a node */
 create table node_photo (
   id int not null primary key auto_increment,
   url varchar(255) not null,
   name varchar(255),
   size int
+);
+
+/* Keep track of downloaded photos */
+create table downloaded_photos (
+  file_path varchar(255) not null primary key,
+  name varchar(255),
+  photo_url varchar(255),
+  width int,
+  height int,
+  type varchar(20)
+);
+
+/* system preferences table */
+create table system_preferences (
+  num_columns int default 2,
+  page_size int default 100,
+  default_display_height int default 500,
+  default_display_width int default 200,
+);
+
+/* Nodes that are not to be searched */
+create table node_exclusion (
+  node_url varchar(255)
+);
+
+/* User specified filter criteria for photos */
+create table photo_filter (
+  filter_criteria varchar (255)
 );
