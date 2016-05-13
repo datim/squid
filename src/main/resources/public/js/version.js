bd36a52504b7663cdb1f2d2e4679dbe76f474046
Datim <jroecks@gmail.com>
2016-05-12 23:48:20 -0700

    Format thumbnails for photos

--- src/main/resources/public/js/version.js
+++ src/main/resources/public/js/version.js
@@ -37,12 +37,32 @@ function reportResults() {
 
   var photoResults = JSON.parse(xhr.responseText);
 
+/*
+  var photoDiv = document.getElementById("PhotoResultsDiv");
+
+  // add new sub-divisions containing photos
+  var i = 0;
+  for (i = 0; i < photoResults.length; i++) {
+
+      var image = document.createElement('img')
+      image.src = photoResults[i].url;
+      image.width = 100;
+      image.height = 100;
+
+      var thumbDiv = document.createElement('div');
+      thumbDiv.className = "nailthumb-container square"
+      thumbDiv.appendChild(image);
+
+      photoDiv.appendChild(thumbDiv);
+  }
+  */
+
   var table = document.getElementById("PhotoResultsTable");
 
   // write a table of photos
-  var photosPerRow = 2;
-  var i = 0;
+  var photosPerRow = 4;
   var row = table.insertRow();
+  var i = 0;
   for (i = 0; i < photoResults.length; i++) {
 
     // create a new row after all the photos have been exhausted
@@ -50,14 +70,31 @@ function reportResults() {
       row = table.insertRow();
     }
 
-    insertTableRowCell(row, photoResults[i].url);
+    insertTableRowCell(row, photoResults[i]);
   }
 }
 
 // insert a photo into a table row cell
-function insertTableRowCell(row, photoURL) {
+function insertTableRowCell(row, photoResult) {
+
+  var image = document.createElement('img');
+  image.src = photoResult.url;
+  image.width = 200;
+  image.height = 200;
+
+  var thumbDiv = document.createElement('PhotoDiv');
+  thumbDiv.appendChild(image);
+
   var cell = row.insertCell();
+  cell.appendChild(thumbDiv);
+
+  /*
   var image = document.createElement('img');
   image.src = photoURL;
+  image.width = 200;
+  image.height = 200;
+
+  var cell = row.insertCell();
   cell.appendChild(image);
+  */
 }
