c3b5b82252aad8247ef726b457d540c07a399016
Datim <jroecks@gmail.com>
2016-05-28 11:49:49 -0700

    Add react fixed data table

---
+++ src/main/resources/public/js/reactgrid.js
@@ -0,0 +1,43 @@
+
+const React = require('react');
+const {Table, Column, Cell} = require('fixed-data-table');
+const ReactDOM = require('react-dom');
+
+class MyTable extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      myTableData: [
+        {name: 'Rylan'},
+        {name: 'Amelia'},
+        {name: 'Estevan'},
+        {name: 'Florence'},
+        {name: 'Tressa'},
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <Table
+        rowsCount={this.state.myTableData.length}
+        rowHeight={50}
+        headerHeight={50}
+        width={1000}
+        height={500}>
+        <Column
+          header={<Cell>Name</Cell>}
+          cell={props => (
+            <Cell {...props}>
+              {this.state.myTableData[props.rowIndex].name}
+            </Cell>
+          )}
+          width={200}
+        />
+      </Table>
+    );
+  }
+}
+
+ReactDOM.render(<MyTable/>, document.getElementById('PhotoResultsDiv2'));
