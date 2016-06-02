b58859bfa2a21c823614e8a31d3f81f55ee6c3ab
Datim <jroecks@gmail.com>
2016-06-01 23:05:37 -0700

    Add cross-origin configuration

---
+++ src/main/java/com/squid/SquidConfiguration.java
@@ -0,0 +1,24 @@
+package com.squid;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class SquidConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+    	
+    	/**
+    	 * Allow cross mappings from port 7777
+    	 */
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/crawl/**").allowedOrigins("http://localhost:7777");
+            }
+        };
+    }
+}
