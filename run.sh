ee9a080d21bafc1c5a2ca5a7f5ceb1fa995fa7e2
Datim <jroecks@gmail.com>
2017-04-19 23:48:19 -0700

    Cleanup run script

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-java -DROOT="C:/users/datim/Desktop/squid" -DLOG_LEVEL=info -jar build/libs/squid-1.0.jar
+#!/bin/bash
+# Execute the program
+LOG_LEVEL=info
+ROOT_DIR="C:/users/datim/Desktop/squid"
+
+java -DROOT=${ROOT_DIR} -DLOG_LEVEL=${LOG_LEVEL} -jar build/libs/squid-1.0.jar
